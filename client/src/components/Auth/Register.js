5292543f228b792a9d6b0ae4641337a7def1524f
Amanda Garcia <algarcian@outlook.com>
2019-06-25 12:16:26 -0600

    Register comp with hooks

--- client/src/components/Auth/Register.js
+++ client/src/components/Auth/Register.js
@@ -1,30 +1,36 @@
-import React, {useState, useContext} from 'react';
-import {  AuthContext } from "../../providers/AuthProvider"; 
+import React, {useState, useContext}from 'react';
+import { AuthContext, } from "../../providers/AuthProvider";
 import { Button, Form, Segment, Header, } from 'semantic-ui-react';
 
-
-const Login = (props) =>{
+const Register = (props) =>{
   const emptyForm ={
     email: "",
     password: "", 
+    passwordConfirmation:"",
   }
-  const [form, setForm] = useState(emptyForm);  
-  const {handleLogin,} = useContext(AuthContext); 
+  const [form, setForm] = useState(emptyForm)
+  const {handleRegister,} = useContext(AuthContext)
 
   
   const handleSubmit = (e) => {
     e.preventDefault()
-    handleLogin({...form}, props.history, handleError)
+    const email = form.email
+    const password = form.password
+    const passwordConfirmation = form.passwordConfirmation
+    if(password === passwordConfirmation){
+      handleRegister({email, password, passwordConfirmation}, props.history)
+    }
+    else alert('Passwords Do Not Match!')
   }
+
 
   const handleChange = (name) => (e) => {
     setForm({...form, [name]: e.target.value})
   }
 
-
   return(
     <Segment basic>
-      <Header as='h1' textAlign='center'>Login</Header>
+      <Header as='h1' textAlign='center'>Register</Header>
       <Form onSubmit={handleSubmit}>
         <Form.Input
           label="Email"
@@ -44,6 +50,15 @@ const Login = (props) =>{
           type='password'
           onChange={handleChange('password')}
         />
+         <Form.Input
+            label="Password Confirmation"
+            required
+            name='passwordConfirmation'
+            value={form.passwordConfirmation}
+            placeholder='Password Confirmation'
+            type='password'
+            onChange={handleChange('passwordConfirmation')}
+          />
         <Segment textAlign='center' basic>
           <Button primary type='submit'>Submit</Button>
         </Segment>
@@ -52,4 +67,4 @@ const Login = (props) =>{
   )
 
 }
-export default Login; 
+export default Register
