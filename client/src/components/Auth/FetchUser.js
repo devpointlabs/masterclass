18ccfe16fd3cdb2b80707943fd15fc8d35650755
Amanda Garcia <algarcian@outlook.com>
2019-06-25 11:57:33 -0600

    Working on Login Comp w/hooks

--- client/src/components/Auth/FetchUser.js
+++ client/src/components/Auth/FetchUser.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState, useContext} from 'react';
+import {useEffect, useState, useContext} from 'react';
 import axios from 'axios';
 import { AuthContext} from '../../providers/AuthProvider';
 
