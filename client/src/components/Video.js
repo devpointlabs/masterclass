2c5a5a5590c942c26085570590bc46158ad56d9b
Matthew Frazier <matthewfrazier1988@gmail.com>
2019-06-25 14:39:52 -0600

    seeded db

--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -1,12 +1,17 @@
 import React from "react";
+import axios from "axios";
 import { Header, } from "semantic-ui-react";
 import { Link, } from "react-router-dom";
 
-const Video = () => (
+const Video = (props) => {
+  
+
+  return(
   <Header as="h3" textAlign="center">
     Video
     <Link to="/"> Home</Link>
   </Header>
-);
+  );
+};
 
 export default Video;
