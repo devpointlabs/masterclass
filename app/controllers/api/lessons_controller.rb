67bcea7c38011ff0846689eaf4ca6f79bf9d15e2
Matthew Frazier <matthewfrazier1988@gmail.com>
2019-06-25 12:05:04 -0600

    lesson controller completed

--- app/controllers/api/lessons_controller.rb
+++ app/controllers/api/lessons_controller.rb
@@ -1,16 +1,45 @@
 class Api::LessonsController < ApplicationController
+  before_action :set_course
+  before_action :set_lesson, only: [:show, :update, :destroy]
+
   def index
+    render json: @course.lessons
   end
 
   def show
   end
 
   def create
+    lesson = @course.lessons.new(lesson_params)
+    if lesson.save
+      render json: lesson
+    else
+      render json: lesson.errors, status: 422
+    end
   end
 
   def update
+    if @lesson.update(lesson_params)
+      render json: @lesson
+    else
+      render json: lesson.errros, status: 422
+    end
   end
 
   def destroy
+    @lesson.destroy
+  end
+
+  private
+  def set_course
+    @course = Course.find(params[:course_id])
+  end
+
+  def set_lesson
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def lesson_params
+    params.require(:lesson).permit(:name, :description)
   end
 end
