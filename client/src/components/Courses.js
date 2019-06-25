3211b44ea2784a4b1e32ea8f32261c603c57bb5d
Amanda Garcia <algarcian@outlook.com>
2019-06-25 17:10:13 -0600

    basic skeleton for landing pg

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -1,7 +1,8 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect} from "react";
 import { Header, Card } from "semantic-ui-react";
 import Course from "./Course";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Courses = (props) => {
   const [
@@ -9,11 +10,29 @@ const Courses = (props) => {
     setCourses
   ] = useState([]);
 
+  useEffect(() => {
+    axios.get("/api/courses")
+    .then(res => {
+      // console.log(res.data)
+      setCourses(res.data)
+    })
+
+  }, [])
+
+
   return (
     <Fragment>
-      {courses.map((course) => (
-        <Card>
-          <Course key={course.id} />
+      {courses.map((item) => (
+        <Card key={item.id}>
+          <Card.Content textAlign="center">
+            {item.image} 
+            Image Goes Here
+          </Card.Content>
+          <Card.Header as="h3">{item.title}</Card.Header>
+          <Card.Meta> 
+            Overview goes here
+            {item.overview}
+          </Card.Meta>
         </Card>
       ))}
     </Fragment>
