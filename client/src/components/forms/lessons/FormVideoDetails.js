1b7aa222c177a50c0412b051a87f5481ca1def8a
Amanda Garcia <algarcian@outlook.com>
2019-07-02 19:03:52 -0600

    Added multiform functionality

---
+++ client/src/components/forms/lessons/FormVideoDetails.js
@@ -0,0 +1,160 @@
+import React, {Fragment, useState, useEffect, useCallback }from 'react'; 
+import {Form, Button} from 'semantic-ui-react'
+import axios from 'axios'
+import styled from 'styled-components';
+import { useDropzone } from 'react-dropzone';
+
+
+
+const FormVideoDetails = (props) => {
+  const [title, setTitle] = useState();
+  const [description, setDescription] = useState();
+  const [file, setFile] = useState();
+
+
+ const continueStep = (e) => {
+    // e.preventDefault(); 
+    props.nextStep(); 
+  }
+ const backStep = (e) => {
+    // e.preventDefault(); 
+    props.previousStep(); 
+  }
+
+  // populate video title and video url. 
+  useEffect((e) => {
+    // if (props.match.params.lesson_id) {
+    //   const { lesson_id } = props.match.params
+    //   axios
+    //     .get(`/api/lessons/${lesson_id}`)
+    //     .then(res => {
+    //       setName(res.data.name)
+    //       setDescription(res.data.description)
+
+    //     })
+    // }
+
+  }, [])
+
+  const handleSubmit = e => {
+
+    e.preventDefault();
+    // if (props.match.params.lesson_id) {
+    //   axios
+    //     .put(`/api/lessons/${props.match.params.lesson_id}`, { name: name, description: description })
+    //     .then(res => {
+    //       // props.history.push(`/courses/${res.data.course_id}`)
+    //       // instead of pushing back to courses, it will push to the next form component
+    //       continueStep(); 
+    //     })
+    // }
+    // else {
+    //   axios
+    //     .post("/api/courses/${course_id}/lessons", { name: name, description: description })
+    //     .then(res => {
+    //       console.log(res.data)
+    //       // add(res.data);
+
+    //     });
+    // };
+  }
+
+
+    // styled component functionality 
+   const StyledDropzone = (props) => {
+      // taken from docs 
+      const onDrop = useCallback(acceptedFiles => {
+        // Do something with the files
+        setFile(acceptedFiles[0]); 
+      }, [])
+
+    const {
+      getRootProps,
+      getInputProps,
+      isDragActive,
+      isDragAccept,
+      isDragReject
+    } = useDropzone({onDrop});
+
+    return (
+      <div className="container">
+        <Container {...getRootProps({isDragActive, isDragAccept, isDragReject})}>
+          <input {...getInputProps()} />
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        </Container>
+      </div>
+    );
+  }
+
+
+
+
+  return (
+    <Fragment>
+    <Form onSubmit={handleSubmit}>
+      <Form.Group widths='equal'>
+        <Form.Input
+          label='Title'
+          placeholder='What best describes the content of the video?'
+          name='title'
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+
+        />
+        {/* TODO: Turn to textarea - keeps saying that value is not a valid prop */}
+        <Form.Input
+          label='Description'
+          placeholder='Give a short summary of the content of the video...'
+          name='description'
+          required
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+      </Form.Group>
+      <br />
+      {/* render all existing videos */}
+      <StyledDropzone />
+      <br />
+      <Form.Button>Continue</Form.Button>
+      <Button onClick={() => backStep()}>Back</Button>
+    </Form>
+
+  </Fragment>
+  )
+}
+
+
+// styles for StyledDropZone
+const getColor = (props) => {
+  if (props.isDragAccept) {
+      return '#00e676';
+  }
+  if (props.isDragReject) {
+      return '#ff1744';
+  }
+  if (props.isDragActive) {
+      return '#2196f3';
+  }
+  return '#bdbdbd';
+}
+
+const Container = styled.div`
+  flex: 1;
+  text-align: center; 
+  height: 250px; 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  border-width: 3px;
+  border-radius: 2px;
+  border-color: ${props => getColor(props)};
+  border-style: dashed;
+  background-color: #eeee;
+  color: #bdbdbd;
+  outline: none;
+  transition: border .24s ease-in-out;
+`;
+
+export default FormVideoDetails;
---
+++ client/src/components/forms/lessons/LessonForm.js
@@ -0,0 +1,187 @@
+import React, { Fragment, useState, useEffect, useCallback } from 'react';
+import axios from 'axios';
+import { Form, } from 'semantic-ui-react';
+import { useDropzone } from 'react-dropzone';
+import FormLessonDetails from './FormLessonDetails'; 
+import FormVideoDetails from './FormVideoDetails'; 
+import styled from 'styled-components';
+
+
+const LessonForm = (props) => {
+  const [step, setStep] = useState(1)
+  // const [name, setName] = useState();
+  // const [description, setDescription] = useState();
+  const [file, setFile] = useState();
+
+
+// proceed to the nextStep 
+const nextStep = () => {
+  setStep(step + 1)
+}
+
+// go back to previous step 
+const previousStep = () => {
+  setStep(step - 1)
+}
+
+// handle state changes - DELETE
+// const handleChange = (input) = (event) => {
+//   setName({[input]: event.target.value}); 
+//   setDescription({[input]: event.target.value}); 
+// }
+
+
+
+  // check if form is editing and render different title and all existing lesson videos with options to delete each video. 
+  // populate video title and video url. 
+  // useEffect((e) => {
+  //   if (props.match.params.lesson_id) {
+  //     const { lesson_id } = props.match.params
+  //     axios
+  //       .get(`/api/lessons/${lesson_id}`)
+  //       .then(res => {
+  //         setName(res.data.name)
+  //         setDescription(res.data.description)
+
+  //       })
+  //   }
+
+  // }, [])
+
+  // const handleSubmit = e => {
+
+  //   e.preventDefault();
+  //   if (props.match.params.lesson_id) {
+  //     axios
+  //       .put(`/api/lessons/${props.match.params.lesson_id}`, { name: name, description: description })
+  //       .then(res => {
+  //         props.history.push(`/courses/${res.data.course_id}`)
+  //         // props.edit(res.data);
+  //       })
+  //   }
+
+  //   else {
+  //     axios
+  //       .post("/api/courses/${course_id}/lessons", { name: name, description: description })
+  //       .then(res => {
+  //         console.log(res.data)
+  //         // add(res.data);
+
+  //       });
+  //   };
+  // }
+
+
+
+
+  // styled component functionality 
+  //  const StyledDropzone = (props) => {
+  //     // taken from docs 
+  //     const onDrop = useCallback(acceptedFiles => {
+  //       // Do something with the files
+  //       setFile(acceptedFiles[0]); 
+  //     }, [])
+
+  //   const {
+  //     getRootProps,
+  //     getInputProps,
+  //     isDragActive,
+  //     isDragAccept,
+  //     isDragReject
+  //   } = useDropzone({onDrop});
+
+  //   return (
+  //     <div className="container">
+  //       <Container {...getRootProps({isDragActive, isDragAccept, isDragReject})}>
+  //         <input {...getInputProps()} />
+  //         <p>Drag 'n' drop some files here, or click to select files</p>
+  //       </Container>
+  //     </div>
+  //   );
+  // }
+switch(step) {
+  case 1: 
+  return (
+    <FormLessonDetails {...props} nextStep={nextStep} previousStep={previousStep}/>
+  )
+  case 2: 
+  return (
+    <FormVideoDetails {...props} nextStep={nextStep} previousStep={previousStep}/>
+  )
+  case 3: 
+  return(
+    <h1>Confirm Form</h1>
+  )
+  case 4: 
+  return(
+    <h1>Success</h1>
+  )
+}
+  // return (
+  //   <Fragment>
+  //     <Form onSubmit={handleSubmit}>
+  //       <Form.Group widths='equal'>
+  //         <Form.Input
+  //           label='Name'
+  //           placeholder='What best describes what will be taught?'
+  //           name='name'
+  //           required
+  //           value={name}
+  //           onChange={(e) => setName(e.target.value)}
+
+  //         />
+  //         {/* TODO: Turn to textarea - keeps saying that value is not a valid prop */}
+  //         <Form.Input
+  //           label='Description'
+  //           placeholder='What will the lesson cover? '
+  //           name='description'
+  //           required
+  //           value={description}
+  //           onChange={(e) => setDescription(e.target.value)}
+  //         />
+  //       </Form.Group>
+  //       <br />
+  //       {/* render all existing videos */}
+  //       {/* <StyledDropzone /> */}
+  //       <br />
+  //       <Form.Button fluid>Submit</Form.Button>
+  //     </Form>
+
+  //   </Fragment>
+  // )
+}
+
+
+// styles for StyledDropZone
+// const getColor = (props) => {
+//   if (props.isDragAccept) {
+//       return '#00e676';
+//   }
+//   if (props.isDragReject) {
+//       return '#ff1744';
+//   }
+//   if (props.isDragActive) {
+//       return '#2196f3';
+//   }
+//   return '#bdbdbd';
+// }
+
+// const Container = styled.div`
+//   flex: 1;
+//   text-align: center; 
+//   height: 250px; 
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   padding: 20px;
+//   border-width: 3px;
+//   border-radius: 2px;
+//   border-color: ${props => getColor(props)};
+//   border-style: dashed;
+//   background-color: #eeee;
+//   color: #bdbdbd;
+//   outline: none;
+//   transition: border .24s ease-in-out;
+// `;
+
+export default LessonForm; 
