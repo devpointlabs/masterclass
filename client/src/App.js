0ad6a6c2c2063aa5bc3f0b634f825ca00e977b0f
Melissa <jstmac@gmail.com>
2019-07-08 13:55:46 -0600

    user profile

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,8 @@ import Navbar from "./components/layout/Navbar";
 import Enrollment from './components/Enrollment'
 import FetchUser from "./components/auth/FetchUser"
 import LessonForm from './components/LessonForm';
-// import ProtectedRoute from "./components/ProtectedRoute";
+import Profile from './components/Profile'
+import ProtectedRoute from "./components/auth/ProtectedRoute";
 
 
 const App = () => (
@@ -22,15 +23,16 @@ const App = () => (
     <FetchUser>
       <Container>
         <Switch>
-          {/* <ProtectedRoute exact path="/" component={Home} /> */}
+          <ProtectedRoute exact path="/" component={Home} />
+          <ProtectedRoute exact path='/profile' component={Profile} />
           <Route exact path="/courses/:id" component={Course} />
           <Route exact path="/" component={Home} />
           <Route exact path="/my-courses" component={Enrollment} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
           {/* <Route exact path="/:course_id/lesson_form/:lesson_id/edit" component={LessonForm}/> */}
-          <Route exact path = "/"/>
-          <Route exact path= "/lessons/:id" component={Lesson}/>
+          <Route exact path="/" />
+          <Route exact path="/lessons/:id" component={Lesson} />
           <Route exact path="/edit_lesson/:lesson_id" component={LessonForm} />
           <Route exact path="/lessons/:lesson_id/videos/:video_id" component={Video} />
           <Route component={NoMatch} />
