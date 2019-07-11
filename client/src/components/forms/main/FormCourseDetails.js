501a1aa64fd3afc10a843872a711b551abae4608
Melissa <jstmac@gmail.com>
2019-07-11 16:01:55 -0600

    create a course cancel button

--- client/src/components/forms/main/FormCourseDetails.js
+++ client/src/components/forms/main/FormCourseDetails.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
-import { Form, Header } from "semantic-ui-react";
-import {AuthContext} from "../../../providers/AuthProvider"; 
+import { Form, Header, Button } from "semantic-ui-react";
+import { AuthContext } from "../../../providers/AuthProvider";
 
 const FormCourseDetails = (props) => {
   const [title, setTitle] = useState()
   const [category, setCategory] = useState()
   const [overview, setOverview] = useState()
   const [image, setImage] = useState()
-  const [course, setCourse] = useState(); 
+  const [course, setCourse] = useState();
 
 
   // useEffect(() => {
@@ -28,7 +29,7 @@ const FormCourseDetails = (props) => {
 
   const continueStep = (e) => {
     // e.preventDefault(); 
-    props.nextStep(); 
+    props.nextStep();
   }
 
   const handleSubmit = e => {
@@ -42,61 +43,72 @@ const FormCourseDetails = (props) => {
     // }
 
     // else {
-      axios
-        .post("/api/courses", { title: title, category: category, overview: overview, image: image })
-        .then(res => {
-          setCourse(res.data);
-          props.getCourseId(res.data.id); 
-          continueStep(); 
-        });
+    axios
+      .post("/api/courses", { title: title, category: category, overview: overview, image: image })
+      .then(res => {
+        setCourse(res.data);
+        props.getCourseId(res.data.id);
+        continueStep();
+      });
     // };
   }
 
 
   return (
     <>
-    <Header as="h1" textAlign="center">Create Your Course</Header>
-    <Form onSubmit={handleSubmit}>
-      <Form.Group widths='equal'>
-        <Form.Input
-          label='Title'
-          placeholder='Please enter a title...'
-          name='title'
-          required
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
+      <Header as="h1" textAlign="center">Create Your Course</Header>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group widths='equal'>
+          <Form.Input
+            label='Title'
+            placeholder='Please enter a title...'
+            name='title'
+            required
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
 
-        />
-        <Form.Input
-          label='Category'
-          placeholder='What field of study?'
-          name='category'
-          required
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-        />
-        <Form.Input
-          label='Overview'
-          placeholder='What will be covered?'
-          name='overview'
-          required
-          value={overview}
-          onChange={(e) => setOverview(e.target.value)}
-        />
-        <Form.Input
-          label='Image'
-          placeholder='Put a thumbnail!'
-          name='image'
-          required
-          value={image}
-          onChange={(e) => setImage(e.target.value)}
-        />
-      </Form.Group>
-      
-      <Form.Button color="green" inverted>Continue</Form.Button>
-    </Form >
+          />
+          <Form.Input
+            label='Category'
+            placeholder='What field of study?'
+            name='category'
+            required
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          />
+          <Form.Input
+            label='Overview'
+            placeholder='What will be covered?'
+            name='overview'
+            required
+            value={overview}
+            onChange={(e) => setOverview(e.target.value)}
+          />
+          <Form.Input
+            label='Image'
+            placeholder='Put a thumbnail!'
+            name='image'
+            required
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Button color="green" inverted>Continue</Form.Button>
+
+        <Button onClick={() => props.history.goBack()} color="red" inverted>Cancel</Button>
+
+      </Form >
     </>
   );
 };
 
 export default FormCourseDetails;
+
+
+{/* <Link to={'/'}>
+  <Button color="black">
+    <Icon name='arrow alternate circle left outline' />
+    Go Back
+          </Button>
+</Link> */}
