e5cc4341948334c886c0dbeee9dd0145548877bb
Patrick Lamoureux <patrick@blackcedar.com>
2019-07-12 12:24:25 -0600

    cloudinary works

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -11,10 +11,7 @@ class Api::UsersController < ApplicationController
     if file
       begin
         ext = File.extname(file.tempfile)
-        cloud_image = Cloudinary::Uploader.upload(
-          file, 
-          public_id: file.original_filename, 
-          secure: true)
+        cloud_image = Cloudinary::Uploader.upload(file, public_id: file.original_filename, secure: true)
         user.image = cloud_image['secure_url']
       rescue => e
         render json: { errors: e }, status: 422
