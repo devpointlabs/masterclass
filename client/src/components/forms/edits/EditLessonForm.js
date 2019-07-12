f4995ee5b7a46416c27630f864f7e509f922d16a
Amanda Garcia <algarcian@outlook.com>
2019-07-12 15:49:50 -0600

    Fixed create lessons

--- client/src/components/forms/edits/EditLessonForm.js
+++ client/src/components/forms/edits/EditLessonForm.js
@@ -2,25 +2,39 @@ import React, {useEffect, useState} from 'react';
 import axios from 'axios'; 
 import {Link} from 'react-router-dom';
 import {Segment, List, Button} from 'semantic-ui-react'; 
-import Course from '../../Course'; 
+import Course from '../../Course';
+import AddLessons from '../adds/AddLessons'; 
 
 const EditLessonForm = (props) => {
   const [lessons, setLessons] = useState([]); 
   const [loader, setLoader] = useState(true)
   const {course_id} = props.match.params; 
+  const [toggleForm, setToggleForm] = useState(false); 
+  const [showCreateForm, setShowCreateForm] = useState(false); 
 
 
-  // axios to get lessons from course
+  
+  
+  // will toggle the add lessons 
+  const toggleCreateForm = () => {
+    setShowCreateForm(!showCreateForm); 
+  }
+  
+  // axios to get lessons from course and will update whenever the toggle goes on and off in order to render the new courses. 
   useEffect(() => {
     axios.get(`/api/courses/${course_id}/lessons`)
     .then(res => {
       setLessons(res.data);
       // setLoader(true)
     })
-  }, [])
+  }, [showCreateForm])
+  // will render the add form for the lessons and passes in the course id
+  const renderAddForm = () => {
+    return  <AddLessons course_id={course_id} showCreateForm={showCreateForm} setShowCreateForm={setShowCreateForm}/>
+  }
+
 
   const renderLessons = () => {
-    debugger 
     if (loader === true) {
       return lessons.map(l => (
          <Segment key={l.id} style={{ display: "flex", justifyContent: "space-between" }}>
@@ -47,7 +61,8 @@ const EditLessonForm = (props) => {
 
   return (
     <div>
-      {/* <Button>Create Lesson</Button> */}
+      <Button onClick={() => toggleCreateForm()}>Create Lesson</Button>
+      {showCreateForm ? renderAddForm() : renderLessons ()}
       {renderLessons()}
     </div>
   )
