d98b82943d81afdda9f823c451f1d5f2f1946958
Amanda Garcia <algarcian@outlook.com>
2019-07-13 11:17:24 -0600

    Moved add form functionality

--- client/src/components/forms/edits/EditLessonForm.js
+++ client/src/components/forms/edits/EditLessonForm.js
@@ -1,15 +1,15 @@
 import React, {useEffect, useState} from 'react'; 
 import axios from 'axios'; 
 import {Link} from 'react-router-dom';
-import {Segment, List, Button} from 'semantic-ui-react'; 
+import {Segment, List, Button, Icon } from 'semantic-ui-react'; 
 import Course from '../../Course';
 import AddLessons from '../adds/AddLessons'; 
 
 const EditLessonForm = (props) => {
   const [lessons, setLessons] = useState([]); 
-  const [loader, setLoader] = useState(true)
+  const [loader, setLoader] = useState(true); 
   const {course_id} = props.match.params; 
-  const [toggleForm, setToggleForm] = useState(false); 
+  const [showEditForm, setShowEditForm] = useState(false); 
   const [showCreateForm, setShowCreateForm] = useState(false); 
 
 
@@ -19,6 +19,9 @@ const EditLessonForm = (props) => {
   const toggleCreateForm = () => {
     setShowCreateForm(!showCreateForm); 
   }
+  const toggleEditForm = () => {
+    setShowEditForm(!showEditForm); 
+  }
   
   // axios to get lessons from course and will update whenever the toggle goes on and off in order to render the new courses. 
   useEffect(() => {
@@ -27,10 +30,17 @@ const EditLessonForm = (props) => {
       setLessons(res.data);
       // setLoader(true)
     })
-  }, [showCreateForm])
+  }, [showCreateForm, showEditForm])
+
+ 
   // will render the add form for the lessons and passes in the course id
   const renderAddForm = () => {
     return  <AddLessons course_id={course_id} showCreateForm={showCreateForm} setShowCreateForm={setShowCreateForm}/>
+  }
+
+  //  will render the edit form for the lessons 
+  const renderEditForm = () => {
+  return <h1>Worked! </h1>
   }
 
 
@@ -39,7 +49,7 @@ const EditLessonForm = (props) => {
       return lessons.map(l => (
          <Segment key={l.id} style={{ display: "flex", justifyContent: "space-between" }}>
          <div>
-           <Link to = {`/lessons/${l.id}`}> 
+        <Link to = {`/lessons/${l.id}`}> 
          <List.Header as="h3">{l.name}</List.Header>
          <List.Description>
            {l.description}
@@ -48,10 +58,12 @@ const EditLessonForm = (props) => {
          {/* <Button size="tiny" color="red" onClick={() => removeLesson(l.id)}>
            <Icon name="trash alternate outline" />
          </Button> */}
-         {/* <Link to={`/edit_lesson/${l.id}`}> <Button size="tiny" color="blue">
+         {/* <Link to={`/edit_lesson/${l.id}`}>  */}
+         <Button size="tiny" color="blue" onClick={() => toggleEditForm()}>
            <Icon name="edit" />
          </Button>
-         </Link> */}
+         {/* { showEditForm ? renderEditForm() : renderLessons()} */}
+         {/* </Link> */}
        </div>
          </Segment>
        
