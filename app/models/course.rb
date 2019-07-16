eb03f5fc880ba6b44475f42818393283ae6c55bc
Melissa <jstmac@gmail.com>
2019-07-16 17:18:37 -0600

    add course categories dropdown from database sql

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,11 @@ class Course < ApplicationRecord
   has_many :users, through: :enrollments
   has_many :lessons, dependent: :destroy
 
+  def self.get_categories
+    find_by_sql(
+      ["select distinct category
+      from courses"]
+    )
+  end
+
 end
