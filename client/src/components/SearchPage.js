684e26a65f1852f8a7290c8d6d7f67586f6361d1
Amanda Garcia <algarcian@outlook.com>
2019-07-17 20:11:05 -0600

    Renders information

--- client/src/components/SearchPage.js
+++ client/src/components/SearchPage.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, {useContext} from 'react'; 
+import {AuthContext} from '../providers/AuthProvider';
+import { Container, Card, Image, Icon } from 'semantic-ui-react';
 
 const SearchPage = () => {
+  const { searchResults } = useContext(AuthContext)
+  const defaultImage = 'https://icon-library.net//images/no-image-icon/no-image-icon-13.jpg'
+
+
   return (
-    <div>
-      <h1>Works!</h1>
-    </div>
+    <Container>
+      {searchResults.map(result => (
+        <Card fluid key={result.id}>
+          <Image src={result.image || defaultImage} size="tiny" floated="left"/>
+          <Card.Header textAlign="center">{result.title}</Card.Header>
+          <p>{result.overview}</p>
+          <Card.Content extra>{result.category}</Card.Content>
+        </Card>
+      ))}
+    </Container>
   )
 }
 
