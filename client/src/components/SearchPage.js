373094f178b9cf7ec5b72458f843564ff5dba331
Amanda Garcia <algarcian@outlook.com>
2019-07-18 12:11:09 -0600

    got rid of useState

--- client/src/components/SearchPage.js
+++ client/src/components/SearchPage.js
@@ -1,4 +1,4 @@
-import React, {useState, useContext} from 'react'; 
+import React, {useContext} from 'react'; 
 import {SearchContext} from '../providers/SearchProvider';
 import { Container, Card, Image, Icon } from 'semantic-ui-react';
 
