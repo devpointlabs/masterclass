302fff99f8df3b6654409ca527cd067d57ecc286
Patrick Lamoureux <patrick@blackcedar.com>
2019-07-18 11:04:07 -0600

    search results deletes

--- client/src/components/layout/Search.js
+++ client/src/components/layout/Search.js
@@ -4,17 +4,16 @@ import { AuthContext } from "../../providers/AuthProvider";
 import axios from "axios";
 
 const Search = (props) => {
-  const [
-    search,
-    setSearch
-  ] = useState("");
+  const [search, setSearch] = useState("");
   const { searchResults, setSearchResults } = useContext(AuthContext);
   const [showClearButton, setShowClearButton ] = useState(false)
 
   // Search by title, overview, and category
   const searchCourses = (e, search) => {
+    setSearch("")
     e.preventDefault();
-    axios.get(`/api/search_courses?search=${search}`).then((res) => {
+    axios.get(`/api/search_courses?search=${search}`)
+    .then((res) => {
       setSearchResults(res.data);
       props.history.push("/search");
       // showClear() 
@@ -26,6 +25,8 @@ const Search = (props) => {
     setSearchResults([]); 
   }
 
+  
+
   // const showClear = () => {
   //   if(searchResults.length > 0 ) {
   //     setShowClearButton(true) 
@@ -35,7 +36,12 @@ const Search = (props) => {
 
   return (
     <Form onSubmit={props.searchCourses}>
-      <Input placeholder="Search for anything..." onChange={(e) => setSearch(e.target.value)} />
+      <Input 
+      placeholder="Search for anything..." 
+      onChange={(e) => setSearch(e.target.value)} 
+      value = {search}
+      // name = 'search'
+      />
       <Button icon circular floated="right" onClick={(e) => searchCourses(e, search)}>
         <Icon circular name="search" />
       </Button>
