87bb110394a5333de6d3f7440e01c0c4a5019404
Patrick Lamoureux <patrick@blackcedar.com>
2019-07-19 14:46:29 -0600

    teacher qa is working but needs css

--- client/src/components/teachers/TeacherQA.js
+++ client/src/components/teachers/TeacherQA.js
@@ -1,19 +1,25 @@
 import React, { useState, useEffect, useContext, } from 'react';
 import axios from 'axios';
 import { AuthContext, } from '../../providers/AuthProvider';
-import { Accordion, Container, Button } from 'semantic-ui-react';
+import styled from 'styled-components';
+import { Accordion, Container, Button, Icon, Comment, Checkbox,  } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import Video from '../Video'
+import QAndA from '../Comment'
+import CommentForm from '../CommentForm'
+import Replies from '../Replies'
 // import { Accordion, AccordionItem, AccordionItemHeading,
 //   AccordionItemPanel,
 // } from 'react-accessible-accordion';
 
 const TeacherQA = (props) =>{
-
+  const [showReplies, setShowReplies] = useState(false)
   const [enrollments, setEnrollments] = useState([])
   const [myQAs, setMyQAs]= useState([])
   const [render, setRender]= useState(false)
   const [userInfo, setUserInfo] = useState([])
+  const [showForm, setShowForm] = useState(false)
+  const [readComment, setReadComment]= useState(false)
 
   const [comments, setComments] = useState([])
 
@@ -30,8 +36,29 @@ const TeacherQA = (props) =>{
         setMyQAs(res.data)
       })
     }, [enrollments])
-  
-
+    const toggle = () =>{
+      setShowForm(!showForm)
+    }
+    const deleteComment = (c_id) =>{
+      axios.delete(`/api/videos/${props.video_id}/comments/${c_id}`)
+      .then(res =>{
+        setComments(comments.filter(c => c.id !== c_id))
+        })
+    }
+    const editComment = (id, comment) => {
+      const editedComment = comments.map(c => {
+        if (c.id === id) {
+          return comment
+        } 
+        else {
+          return c
+        }
+      })
+      return setComments(editedComment)
+    }
+    const addComment = (comment) =>{
+      setComments([...comments, comment])
+    }
   const courses = Array.from(new Set(myQAs.map(c =>c.c_title))).map(title=>{
     return myQAs.find(c => c.c_title === title)
   })
@@ -45,6 +72,14 @@ const TeacherQA = (props) =>{
 const fillComments = (id) =>{
   axios.get(`/api/videos/${id}/comments`)
   .then(res => setComments(res.data))
+  const stuffRead = comments.map(c =>{
+    return {
+      title: c.title,
+      video_id: c.video_id,
+      readComment: false,
+      body: c.body
+    }
+  })
 }
 
 const videoPanels = (id) =>{
@@ -124,17 +159,77 @@ const coursePanels = () =>{
   const rootPanels = [
     {key: 1, title: 'Courses', content: {content: CourseContent}}
   ]
+  const commentRead = (id) =>{
+    axios.put(`/api/toggle-read/${id}`)
+
+  }
+  const toggleReplies = () => {
+    setShowReplies(!showReplies)
+  }
+  const renderButtons = (read, id) =>{
+    if(!read){
+      return (
+        <Checkbox label="Comment Read" onClick={()=>commentRead(id)}/>
+      )
+    }
+  }
+  const showComments = () => {
+    return (
+      
+      comments.map( c => (
+        <>
+        {!c.read ? (
+          <Comment key={c.id}>
+            <Comment.Content>
+              {c.title}
+            </Comment.Content>
+            <Comment.Content>
+              {c.body}
+            </Comment.Content>
+            <Comment.Action>
+              {renderButtons(c.read, c.id)}
+            </Comment.Action>
+            <ClickDiv>
+          { showReplies ?
+            <RepliesClick onClick={() => toggleReplies()}>Hide Replies <Icon name='angle up' /></RepliesClick>
+            :
+            <RepliesClick onClick={() => toggleReplies()}>Show Replies <Icon name='angle down' /></RepliesClick>
+          }
+        </ClickDiv>
+        <Comment.Group>
+          { showReplies ?
+            <Replies 
+              role={c.role}
+              comment_id={c.id}
+            /> :
+            null
+          }
+        </Comment.Group>
+        </Comment>
+        ): null}
+     
+      </>
+      ))
+    )
+  }
   return (
     <>
     <div style={{display: 'flex', justifyContent:"space-evenly"}}>
     <Accordion defaultActiveIndex={0} panels={rootPanels} styled />
     <div>
-    <h1>Comments</h1>
-      {comments.map(c=>{
-        return (
-          <h4>{c.id}. {c.body}</h4>
-        )
-      })}
+    <h1>Student Questions</h1>
+      <>
+      <div style= {{marginTop: '30px'}}>
+        <hr/>
+     
+        <div style={{display:'flex', justifyContent:'flex-start', marginTop:'30px'}}>
+          <Comment.Group>
+            {showComments()}
+            {console.log(comments)}
+          </Comment.Group>
+        </div>
+      </div>
+    </>
     </div>
   
     </div>
@@ -143,5 +238,21 @@ const coursePanels = () =>{
 
 
 }
+const ClickDiv = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`
+
+const RepliesClick = styled.p`
+  color: blue;
+  border-radius: 5px;
+  padding-left: 7px;
+
+  &:hover {
+    cursor: pointer;
+    background: grey ;
+    transition: background 0.7s ease;
+  } 
+`
 
 export default TeacherQA
