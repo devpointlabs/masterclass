18afdd3d7da1b3cb547279e511fdf6891b64759d
Patrick Lamoureux <patrick@blackcedar.com>
2019-07-23 15:40:24 -0600

    video edit works

--- client/src/components/forms/edits/EditVideoDetails.js
+++ client/src/components/forms/edits/EditVideoDetails.js
@@ -1,11 +1,61 @@
-import React from 'react'
+import React, { Fragment, useState, useEffect } from 'react';
+import { Form, Header, Button } from 'semantic-ui-react'
+import axios from 'axios'
 
-const EditVideoDetails = () => {
+const EditVideoDetails = (props) => {
+  const [title, setTitle] = useState();
+  const [showForm, setShowForm] = useState(false)
+  const [description, setDescription] = useState();
+  const [video, setVideo] = useState();
+
+  useEffect((e) => {
+      axios
+        .get(`/api/lessons/${props.lesson_id}/videos/${props.video_id}`)
+        .then(res => {
+          setTitle(res.data.title)
+          setDescription(res.data.description)
+        })
+    
+  }, [])
+  const handleSubmit = e => {
+    e.preventDefault();
+      axios
+        .put(`/api/lessons/${props.lesson_id}/videos/${props.video_id}`, { title: title, description: description })
+        .then(res => {
+         setVideo(res.data); 
+        })
+    }
   return (
-    <div>
-      <h1>Works!</h1>
-    </div>
+    <Fragment>
+    <Form onSubmit={handleSubmit}>
+      <Form.Group widths='equal'>
+        <Form.Input
+          label='Title'
+          placeholder='What best describes what will be taught?'
+          name='title'
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+
+        />
+        {/* TODO: Turn to textarea - keeps saying that value is not a valid prop */}
+        <Form.Input
+          label='Description'
+          placeholder='What will the lesson cover? '
+          name='description'
+          required
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+      </Form.Group>
+      <Form.Button inverted color="green" >
+        Save
+        </Form.Button>
+    </Form >
+
+  </Fragment >
   )
+ 
 }
 
 export default EditVideoDetails
